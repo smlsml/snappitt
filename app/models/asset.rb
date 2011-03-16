532759e0b4e19109250d60934979ceadd35b771a
Steve Lawson <smlsml@gmail.com>
2011-03-16 13:08:16 -0600

    Attempt to auto-orient images

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -10,7 +10,7 @@ class Asset < ActiveRecord::Base
                                   :feed => "280x157#",
                                   :preview => ['300x300>', :jpg],
                                   :large => ['650x650>', :jpg]},
-                      :convert_options => {:feed => '-gravity center -extent 280x157' },
+                      :convert_options => {:all => '-auto-orient', :feed => '-gravity center -extent 280x157'},
                       :storage => :s3,
                       :s3_credentials => "#{Rails.root}/config/s3.yml",
                       :path => ":id/:style_:extension",
@@ -22,7 +22,20 @@ class Asset < ActiveRecord::Base
                                   :feed => "280x157#",
                                   :preview => ['300x300>', :jpg],
                                   :large => ['650x650>', :jpg]},
-                      :convert_options => {:feed => '-gravity center -extent 280x157' }
+                      :convert_options => {:feed => '-gravity center -extent 280x157'}
+  end
+
+  after_photo_post_process  :post_process_photo
+
+  def post_process_photo
+    imgfile = Magick::Image.read(data.queued_for_write[:original].path).first
+
+    return unless imgfile
+    logger.info "Photo EXIF: " + imgfile.get_exif_by_entry().inspect
+
+    #if logger.info?
+    #  exifDate = imgfile.get_exif_by_entry('DateTime')[0][1]
+    #end
   end
 
 end
