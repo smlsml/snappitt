91090e6dc09b3fb528a490ca5d9f5aa958ad2616
Steve Lawson <smlsml@gmail.com>
2011-03-15 21:50:24 -0600

    Display created_at and source for moments

--- app/models/source.rb
+++ app/models/source.rb
@@ -4,4 +4,10 @@ class Source < ActiveRecord::Base
     self.find_or_create_by_name(request.env['HTTP_USER_AGENT'] || 'unknown')
   end
 
+  def to_s
+    return 'iPhone' if name.to_s.downcase.include?('iphone')
+    return 'Email' if name.to_s.downcase.include?('cloudmailin')
+    return 'Web'
+  end
+
 end
