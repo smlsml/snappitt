f3912768a12bdb65c65888848e659100b2bffdcb
Steve Lawson <smlsml@gmail.com>
2011-03-28 20:35:32 -0600

    Add activity to email notifications

--- test/functional/experience_mailer_test.rb
+++ test/functional/experience_mailer_test.rb
@@ -4,10 +4,10 @@ class ExperienceMailerTest < ActionMailer::TestCase
 
   test "upload_notification" do
     @experience = experiences(:public)
-    mail = ExperienceMailer.upload_notification(@experience)
+    mail = ExperienceMailer.upload_notification(@experience, @experience.creator, false)
     assert_match "Experience", mail.subject
     assert_equal [@experience.creator.email], mail.to
-    assert_equal ["reply-post@snappitt.com"], mail.from
+    assert_equal ["post#{@experience.id}@snappitt.com"], mail.from
     assert_match "experience", mail.body.encoded
   end
 
