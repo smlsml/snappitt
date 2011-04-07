8f54b342e5319594c61488d8676bf79681f28121
Steve Lawson <smlsml@gmail.com>
2011-04-07 14:47:40 -0600

    Fix notifications

--- app/helpers/mail_helper.rb
+++ app/helpers/mail_helper.rb
@@ -9,10 +9,12 @@ module MailHelper
     protected
 
     def subject
-      moment = @cause.subject.respond_to?(:moment) ? @cause.subject.moment : nil
-      moment = @cause.subject if @cause.subject.is_a?(Moment)
-      anchor = moment.nil? ? {} : {:anchor => moment.id}
-      '%s (%s)' % [@cause.subject.class.name, @controller.experience_url(@cause, anchor)]
+      anchor = @cause.subject.is_a?(Moment) ? {:anchor => @cause.subject.to_key} : {}
+      experience = @cause.subject.is_a?(Experience) ? @cause.subject: nil
+      experience = @cause.subject.experience if @cause.subject.try(:experience)
+      link = experience.blank? ? '' : @controller.experience_url(experience, anchor)
+
+      '%s%s' % [@cause.subject.class.name, link]
     end
 
   end
