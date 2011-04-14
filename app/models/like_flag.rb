bcec629d0231e943c76f186d12a390acc7159a51
Steve Lawson <smlsml@gmail.com>
2011-04-13 20:46:06 -0600

    Change up rating

--- app/models/like_flag.rb
+++ app/models/like_flag.rb
@@ -1,6 +1,6 @@
 class LikeFlag < MomentFlag
 
-  SHOTS = ['Artsy','Funny','Hot','Macro','Dark','Boo','Inappropriate']
+  SHOTS = ['Portrait','Artsy','Nice!','Funny','Macro','Huh?','Inappropriate']
 
   belongs_to :user, :inverse_of => :likes, :counter_cache => :likes_count
   belongs_to :moment, :inverse_of => :likes, :counter_cache => :likes_count
