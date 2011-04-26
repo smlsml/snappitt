83cc0ac1856941caf36ab577215efde6e3b37498
Steve Lawson <smlsml@gmail.com>
2011-04-26 16:07:57 -0600

    Try to fix invite

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -3,6 +3,8 @@ class InviteMailer < ActionMailer::Base
   def invite(inv)
     @invite = inv
 
+    p @invite
+
     mail :to => @invite.email,
          #:bcc => @invite.user.email,
          :subject => '%s invites you to help capture your experience' % @invite.user
