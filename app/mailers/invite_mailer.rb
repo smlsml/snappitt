3b3307e7d8d4c95de5a96a42225e368f17461217
Steve Lawson <smlsml@gmail.com>
2011-04-26 16:47:26 -0600

    default from for invites

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -1,9 +1,11 @@
 class InviteMailer < ActionMailer::Base
+  default :from => "noreply@%s" % I18n.translate('app.host')
 
   def invite(inv)
     @invite = inv
 
     p @invite.email
+    p @invite.user.email
 
     mail :to => @invite.email,
          :bcc => @invite.user.email,
