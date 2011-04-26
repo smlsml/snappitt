60d8a6fe011d61a6cb86d0afc6b7d1eacd3c4b12
Steve Lawson <smlsml@gmail.com>
2011-04-26 16:05:15 -0600

    Bugs

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -4,7 +4,7 @@ class InviteMailer < ActionMailer::Base
     @invite = inv
 
     mail :to => @invite.email,
-         :bcc => @invite.user.email,
+         #:bcc => @invite.user.email,
          :subject => '%s invites you to help capture your experience' % @invite.user
   end
 end
